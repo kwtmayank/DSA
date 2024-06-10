621880be53026b48c9bd965f31df314f11a0ba59
Mayank Kumawat <kwtmayank@gmail.com>
2024-06-10 18:53:05 +0530

    count-digits

--- src/maths/BasicMaths.java
+++ src/maths/BasicMaths.java
@@ -2,11 +2,17 @@ package maths;
 
 public class BasicMaths{
     public static void main(String[] args) {
-       // countDigits(7756);
+        countDigits(846213);
     }
 
     public static void countDigits(int n){
-        System.out.println(Integer.valueOf(n).toString().length());
+        int counter = 0;
+        while(n > 0){
+            int rem = n%10;
+            counter++;
+            n = n/10;
+        }
+        System.out.println("Number of digits are "+counter);
     }
 
 
